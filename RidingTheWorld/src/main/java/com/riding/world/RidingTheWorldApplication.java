9e7d6d8c97cfb318167e3b1d124821c7777d4bdb
dangarduv2 <dangarduv2@gmail.com>
2023-03-29 09:05:52 +0200

    Proyecto actualizado

---
+++ RidingTheWorld/src/main/java/com/riding/world/RidingTheWorldApplication.java
@@ -0,0 +1,13 @@
+package com.riding.world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidingTheWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidingTheWorldApplication.class, args);
+	}
+
+}
---
+++ RidingTheWorld/src/test/java/com/riding/world/RidingTheWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riding.world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidingTheWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
