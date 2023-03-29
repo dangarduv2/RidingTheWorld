fb43076b7a36b8732af050b606e2e7f82346b600
dangarduv2 <dangarduv2@gmail.com>
2023-03-29 14:57:31 +0200

    Creando fichero import.sql y añadiendo valores

---
+++ RidingTheWorld/src/main/resources/import.sql
@@ -0,0 +1,12 @@
+insert into marca values (1,'yamaha')
+insert into marca values (2,'kawasaki')
+insert into marca values (3,'honda')
+insert into marca values (4,'zontes')
+insert into marca values (6,'penigale')
+
+insert into Lisencia values (1,'AM')
+insert into Lisencia values (2,'A1')
+insert into Lisencia values (3,'A2')
+insert into Lisencia values (4,'A')
+
+insert into Bike values (1,300,3,4,'Zontes-300')
