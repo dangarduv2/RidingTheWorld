c5ede41debc2f46f610fbcbb1b58242994a4f321
dangarduv2 <dangarduv2@gmail.com>
2023-04-06 12:09:00 +0200

    Demo añadido Clases Controladoras creadas (vacias)

--- demo/src/main/java/com/example/application/resources/ActorResource.java
+++ demo/src/main/java/com/example/application/resources/ActorResource.java
@@ -4,6 +4,9 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,17 +16,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.domains.contracts.services.ActorService;
 import com.example.domains.entities.dtos.ActorDTO;
+import com.example.domains.entities.dtos.ActorShort;
+import com.example.domains.entities.dtos.ElementoDTO;
 import com.example.exceptions.BadRequestException;
 import com.example.exceptions.DuplicateKeyException;
 import com.example.exceptions.InvalidDataException;
 import com.example.exceptions.NotFoundException;
 
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 
 import org.springframework.http.HttpStatus;
@@ -35,8 +42,15 @@ public class ActorResource {
 	private ActorService srv;
 
 	@GetMapping
-	public List<ActorDTO> getAll() {
-		return srv.getByProjection(ActorDTO.class);
+	public List<ActorShort> getAll(@RequestParam(required = false) String sort) {
+		if(sort != null)
+			return (List<ActorShort>)srv.getByProjection(Sort.by(sort), ActorShort.class);
+		return srv.getByProjection(ActorShort.class);
+	}
+	
+	@GetMapping(params = "page")
+	public Page<ActorShort> getAll(Pageable pageable) {
+		return srv.getByProjection(pageable, ActorShort.class);
 	}
 
 	@GetMapping(path = "/{id}")
@@ -45,6 +59,17 @@ public class ActorResource {
 		if(item.isEmpty())
 			throw new NotFoundException();
 		return ActorDTO.from(item.get());
+	}
+	
+	@GetMapping(path = "/{id}/pelis")
+	@Transactional
+	public List<ElementoDTO<Integer, String>> getPelis(@PathVariable int id) throws NotFoundException {
+		var item = srv.getOne(id);
+		if(item.isEmpty())
+			throw new NotFoundException();
+		return item.get().getFilmActors().stream()
+				.map(o -> new ElementoDTO<>(o.getFilm().getFilmId(), o.getFilm().getTitle()))
+				.toList();
 	}
 	
 	@PostMapping
