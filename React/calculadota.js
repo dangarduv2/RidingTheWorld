0989b84a6a983ed878fe8020aeec4531132e61cb
dangarduv2 <dangarduv2@gmail.com>
2023-04-17 14:43:01 +0200

    Suma, resta y resetea

--- React/calculadota.js
+++ React/calculadota.js
@@ -1,20 +1,79 @@
 var numero;
-var numeroAuxiliar;
-var numeroResultado;
+var numeroAuxiliar = 0;
+var numeroResultado = 0;
+var ultimo;
 function boton(valor) {
     var visor = document.getElementById("visor");
     var inputValue = document.getElementById("visor").value;
-    visor.setAttribute('value', "".concat(inputValue) + String(valor));
-    var cadena = "".concat(inputValue) + String(valor);
-    numero = parseInt(cadena);
-    console.log(numeroAuxiliar);
+    if (ultimo != "i") {
+        if (inputValue == "0") {
+            visor.setAttribute('value', String(valor));
+        }
+        else {
+            visor.setAttribute('value', "".concat(inputValue) + String(valor));
+        }
+        var cadena = "".concat(inputValue) + String(valor);
+        numero = parseInt(cadena);
+    }
+    imprimir();
 }
 function btn_suma() {
     var visor = document.getElementById("visor");
-    var inputValue = document.getElementById("visor").value;
-    visor.setAttribute('value', "");
-    numeroAuxiliar = numero;
+    var m = document.getElementById("visor").value;
+    if (m != "" && ultimo != "i") {
+        numeroAuxiliar = numeroAuxiliar + parseInt(m);
+    }
     numero = 0;
-    console.log(numero);
-    console.log(numeroAuxiliar);
+    if (numeroAuxiliar != 0) {
+        visor.setAttribute('value', "");
+    }
+    else {
+        visor.setAttribute('value', String(numeroAuxiliar));
+    }
+    ultimo = "s";
+    imprimir();
+}
+function btn_resta() {
+    var visor = document.getElementById("visor");
+    var m = document.getElementById("visor").value;
+    if (m != "" && ultimo != "i" && numeroAuxiliar != 0) {
+        numeroAuxiliar = numeroAuxiliar - parseInt(m);
+    }
+    numero = 0;
+    if (numeroAuxiliar != 0) {
+        visor.setAttribute('value', "");
+    }
+    else {
+        visor.setAttribute('value', String(numeroAuxiliar));
+    }
+    ultimo = "r";
+    imprimir();
+}
+function btn_igual() {
+    var visor = document.getElementById("visor");
+    var m = document.getElementById("visor").value;
+    if (m != "") {
+        switch (ultimo) {
+            case "s":
+                numeroAuxiliar = numeroAuxiliar + parseInt(m);
+                break;
+            case "r":
+                numeroAuxiliar = numeroAuxiliar - parseInt(m);
+                break;
+        }
+    }
+    visor.setAttribute('value', String(numeroAuxiliar));
+    ultimo = "i";
+    imprimir();
+}
+function reset() {
+    var visor = document.getElementById("visor");
+    visor.setAttribute('value', "0");
+    numero = 0;
+    numeroAuxiliar = 0;
+    ultimo = "res";
+}
+function imprimir() {
+    console.log("Valor de numero:" + numero);
+    console.log("Valor de auxiliar:" + numeroAuxiliar);
 }
--- React/calculadota.ts
+++ React/calculadota.ts
@@ -1,28 +1,105 @@
 
 var numero :number;
-var numeroAuxiliar :number;
-var numeroResultado :number;
-
+var numeroAuxiliar :number =0;
+var numeroResultado :number=0;
+var ultimo :String;
 
 
 function boton(valor:number) {
     var visor:(HTMLElement|null)  = document.getElementById("visor");
     var inputValue = (<HTMLInputElement>document.getElementById("visor")).value;
 
-        visor!.setAttribute('value', `${inputValue}`+ String(valor));
-        var cadena = `${inputValue}`+ String(valor);
 
-        numero = parseInt(cadena);
+    if(ultimo!="i"){
+        if(inputValue=="0"){
+            visor!.setAttribute('value', String(valor));
+    
+        }else{
+            visor!.setAttribute('value', `${inputValue}`+ String(valor));
+        }
+            var cadena = `${inputValue}`+ String(valor);
+    
+            numero = parseInt(cadena);
+    }
+
+
+        imprimir();
 }
 
 
 function btn_suma() {
     var visor:(HTMLElement|null)  = document.getElementById("visor");
-    var inputValue = (<HTMLInputElement>document.getElementById("visor")).value;
+    var m = (<HTMLInputElement>document.getElementById("visor")).value;
 
-    visor!.setAttribute('value', "");
+    if(m!="" && ultimo!="i"){
+        numeroAuxiliar= numeroAuxiliar+parseInt(m);
+    }
     
-    numeroAuxiliar= numero;
+
     numero=0;
+    if(numeroAuxiliar!=0){
+        visor!.setAttribute('value', "");
+
+    }else{
+        visor!.setAttribute('value', String(numeroAuxiliar));
+    }
+
+     ultimo ="s";
+
+     imprimir();
+}
+
+
+function btn_resta() {
+    var visor:(HTMLElement|null)  = document.getElementById("visor");
+    var m = (<HTMLInputElement>document.getElementById("visor")).value;
+
+    if(m!="" && ultimo!="i" && numeroAuxiliar!=0){
+        numeroAuxiliar= numeroAuxiliar-parseInt(m);
+    }
+    
+
+    numero=0;
+    if(numeroAuxiliar!=0){
+        visor!.setAttribute('value', "");
+
+    }else{
+        visor!.setAttribute('value', String(numeroAuxiliar));
+    }
+
+     ultimo ="r";
+
+     imprimir();
+}
+
+
+function btn_igual() {
+    var visor:(HTMLElement|null)  = document.getElementById("visor");
+    var m = (<HTMLInputElement>document.getElementById("visor")).value;
+
+    if(m!=""){
+    switch(ultimo){
+        case "s": numeroAuxiliar= numeroAuxiliar+parseInt(m); break;
+        case "r": numeroAuxiliar= numeroAuxiliar-parseInt(m); break;
+    }
+}
+    visor!.setAttribute('value', String(numeroAuxiliar));
+    
+    ultimo="i";
+    imprimir();
+}
+
+
+function reset(){
+    var visor:(HTMLElement|null)  = document.getElementById("visor");
+    visor!.setAttribute('value', "0");
+     numero =0;
+     numeroAuxiliar =0;
+     ultimo ="res";
+}
+
+function imprimir(){
+    console.log("Valor de numero:"+numero);
+    console.log("Valor de auxiliar:"+numeroAuxiliar);
 }
 
